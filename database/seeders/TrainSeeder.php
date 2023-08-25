15dbeac1f5442f03e75e87d22a6eb15d16e3fa9e
matteo2302 <matteo.pizzichemi01@gmail.com>
2023-08-25 15:58:05 +0200

    to fix seed

--- database/seeders/TrainSeeder.php
+++ database/seeders/TrainSeeder.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\Train;
 use Faker\Generator as Faker;
+
 class TrainSeeder extends Seeder
 {
     /**
@@ -13,22 +14,16 @@ class TrainSeeder extends Seeder
      */
     public function run(Faker $faker): void
     {
-        $new_train = new Train();
-        $new_train-> Stazione_di_partenza= $faker->name();
-        $new_train-> Stazione_di_arrivo= $faker->name();
-        $new_train-> Orario_di_partenza= $faker->;
-        $new_train-> Orario_di_arrivo= $faker->;
-        $new_train-> Codice_Treno= $faker->;
-        $new_train-> Numero_Carrozze= $faker->;
-        $new_train-> In_orario= $faker->;
-        $new_train-> Cancellato= $faker->;
-        
+        for ($i = 0; $i < 40; $i++) {
+            $new_train = new Train();
+            $new_train->Stazione_di_partenza = $faker->name();
+            $new_train->Stazione_di_arrivo = $faker->name();
+            $new_train->Orario_di_partenza = $faker->randomFloat(2, 0, 12);
+            $new_train->Orario_di_arrivo = $faker->randomFloat(2, 12, 0);
+            $new_train->Codice_Treno = $faker->randomFloat(0, 1500, 4000);
+            $new_train->Numero_Carrozze = $faker->numberBetween(10, 24);
+            $new_train->In_orario = $faker->numberBetween(0, 1);
+            $new_train->Cancellato = $faker->numberBetween(0, 1);
+        }
     }
 }
-
-
-
-
-
-
-
